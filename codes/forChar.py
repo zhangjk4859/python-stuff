b338b3efff9b75410df416640cfac858799728a8
zhangjk4859 <573581325@qq.com>
2016-12-09 18:41:25 +0800

    no message

---
+++ codes/forChar.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+for num in range(0,100000):
+	char = chr(num)
+	print(num,char)
---
+++ codes/ifage.py
@@ -0,0 +1,6 @@
+age = 20
+
+if age >= 18 :
+	print('adult')
+else:
+	print('teenage')
